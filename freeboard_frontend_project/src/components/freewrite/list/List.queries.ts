49524cb33632db40b5eaed10465bc23edae4b49d
TaewoongMoon <taewoongmoon1@gmail.com>
2021-06-05 17:33:20 +0900

    feat: Add search function

--- freeboard_frontend_project/src/components/freewrite/list/List.queries.ts
+++ freeboard_frontend_project/src/components/freewrite/list/List.queries.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client'
 
 export const FETCH_BOARDS = gql`
-  query fetchBoards($page: Int) {
-    fetchBoards(page: $page) {
+  query fetchBoards($search: String, $page: Int) {
+    fetchBoards(search: $search, page: $page) {
       title
       createdAt
       writer
@@ -12,8 +12,8 @@ export const FETCH_BOARDS = gql`
 `
 
 export const FETCH_BOARDSCOUNT = gql`
-  query fetchBoardsCount {
-    fetchBoardsCount
+  query fetchBoardsCount($search: String) {
+    fetchBoardsCount(search: $search)
   }
 `
 
