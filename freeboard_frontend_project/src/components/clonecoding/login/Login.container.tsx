71a84d66592d7a98f1dd1b4d61e8e031ab9e1dac
TaewoongMoon <taewoongmoon1@gmail.com>
2021-06-11 04:42:18 +0900

    test

--- freeboard_frontend_project/src/components/clonecoding/login/Login.container.tsx
+++ freeboard_frontend_project/src/components/clonecoding/login/Login.container.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import LoginUI from './Login.presenter'
 
 const LoginPage = () => {
