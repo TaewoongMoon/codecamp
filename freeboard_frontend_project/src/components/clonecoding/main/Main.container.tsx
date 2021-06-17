7d2b9053a0c3ddc88c58ff7ffccb6849e95577fb
TaewoongMoon <taewoongmoon1@gmail.com>
2021-06-17 09:29:50 +0900

    Page: Update MainPage Second Part

--- freeboard_frontend_project/src/components/clonecoding/main/Main.container.tsx
+++ freeboard_frontend_project/src/components/clonecoding/main/Main.container.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
 import withAuth from '../../../commons/hocs/withAuth'
 import CloneMainUI from './Main.presenter'
 const CloneMainPage = () => {
-  return <CloneMainUI></CloneMainUI>
+  const [selectedId, setSelectedId] = useState('favorite')
+
+  const onClickMenuList = (event: any) => {
+    setSelectedId(event.target.id)
+  }
+  return (
+    <CloneMainUI onClickMenuList={onClickMenuList} selectedId={selectedId} />
+  )
 }
 
 export default withAuth(CloneMainPage)
